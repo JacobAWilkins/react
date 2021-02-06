6867a1da1bf910e57daa29a55629430b7f0ea920
Jacob Wilkins <jacobwilkins501@gmail.com>
2021-02-06 13:26:57 -0600

    Added basic routing to news exercise

--- news/src/App.js
+++ news/src/App.js
@@ -1,4 +1,3 @@
-import logo from './tek_logo.png';
 import './App.css';
 import NavBar from './Components/NavBar';
 import Headlines from './Views/Headlines';
@@ -6,26 +5,43 @@ import Sports from './Views/Sports';
 import Weather from './Views/Weather';
 import Politics from './Views/Politics';
 import Footer from './Components/Footer';
+import { useState, useEffect } from 'react';
+import { Switch, Route } from 'react-router-dom';
 
-function App() {
+export default function App() {
+
+  const [news, setNews] = useState([]);
+
+  const NEWS_URL = 'http://newsapi.org/v2/top-headlines?' + 'country=us&' + 'apiKey=1dad88788ed3429592a836fd125189ed';
+  const req = new Request(NEWS_URL);
+  
+  useEffect(() => {    
+    fetch(req)
+      .then(raw => raw.json())
+      .then(res => setNews(res.results));
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <NavBar/>
-        <img src={logo} className="App-logo" alt="logo" />
-        <div>
-         <br/>
-          <p>Create a News Site</p>
-          <p>Set up a Single Page Application (SPA) with react-router</p>
-          <Headlines/>
-          <Sports/>
-          <Weather/>
-          <Politics/>
-        </div>
+        <Switch>
+          <br/>
+          <Route exact path="/">
+            <Headlines/>
+          </Route>
+          <Route exact path="/">
+            <Sports/>
+          </Route>
+          <Route exact path="/">
+            <Weather/>
+          </Route>
+          <Route exact path="/">
+            <Politics/>
+          </Route>
+        </Switch>
       </header>
       <Footer/>
     </div>
   );
 }
-
-export default App;
